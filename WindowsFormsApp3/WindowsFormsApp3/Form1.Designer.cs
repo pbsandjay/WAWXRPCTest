27705f74653cad153477b687521b9433f7a12da4
pbsandjay <48532071+pbsandjay@users.noreply.github.com>
2020-07-08 18:53:05 -0400

    Add files via upload

---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btConnect = new DevComponents.DotNetBar.ButtonX();
+            this.btAmmo = new DevComponents.DotNetBar.ButtonX();
+            this.SuspendLayout();
+            // 
+            // btConnect
+            // 
+            this.btConnect.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btConnect.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.btConnect.Location = new System.Drawing.Point(12, 26);
+            this.btConnect.Name = "btConnect";
+            this.btConnect.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btConnect.Size = new System.Drawing.Size(75, 23);
+            this.btConnect.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btConnect.TabIndex = 0;
+            this.btConnect.Text = "Connect";
+            this.btConnect.Click += new System.EventHandler(this.btConnect_Click);
+            // 
+            // btAmmo
+            // 
+            this.btAmmo.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btAmmo.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.btAmmo.Location = new System.Drawing.Point(12, 121);
+            this.btAmmo.Name = "btAmmo";
+            this.btAmmo.Size = new System.Drawing.Size(75, 23);
+            this.btAmmo.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btAmmo.TabIndex = 1;
+            this.btAmmo.Text = "Max Ammo";
+            this.btAmmo.Click += new System.EventHandler(this.btSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 401);
+            this.Controls.Add(this.btAmmo);
+            this.Controls.Add(this.btConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.ButtonX btConnect;
+        private DevComponents.DotNetBar.ButtonX btAmmo;
+    }
+}
+
---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using XRPCLib;
+using JRPC_Client;
+using DevComponents.DotNetBar;
+using XDevkit;
+using XDevkitPlusPlus;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+
+        XRPC JTAG = new XRPC();
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btConnect_Click(object sender, EventArgs e)
+        {
+                JTAG.Connect();
+                if (JTAG.activeConnection == true)
+                {
+
+                    MessageBoxEx.Show("Connection Successful", "Connected", MessageBoxButtons.OK);
+                }
+
+                else
+                {
+                    MessageBoxEx.Show("Connection Failed", "Not Connected", MessageBoxButtons.OK);
+                }
+            }
+
+        private void btSend_Click(object sender, EventArgs e)
+        {
+            byte[] Values = { 0x00, 0x00, 0x00, 0xF6, };
+            JTAG.SetMemory(0xC458AD94, Values);
+
+        }
+    }
+}
